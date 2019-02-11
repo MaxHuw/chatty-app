f6c1f304bc54924e36a9ed8024e596fc3ae565bc
MaxHuw <m.huw.graham@gmail.com>
2019-02-11 23:48:37 +0000

    Working on setting up initial React Components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import React, {Component} from 'react';
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <MessageList />
+        <ChatBar />
+      </div>
     );
   }
 }
---
+++ src/MesageList.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class MessageList extends Component {
+    render() {
+        return (
+            <main class="messages">
+                <Message />
+            </main>
+        )
+    }
+}
