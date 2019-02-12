62f293d577d205aaa26da896b823d4e880c431e3
MaxHuw <m.huw.graham@gmail.com>
2019-02-12 22:11:13 +0000

    Can change the username in the ChatBar and post new messages.

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,27 +2,34 @@ import React, {Component} from 'react';
 
 class ChatBar extends Component {
 
-    // constructor(props){
-    //     super(props);
+    constructor(props){
+        super(props);
 
-    //     this.state = {
-    //         user: this.props.currentUser,
-    //         comment: ''
-    //     }
+        this.state = {
+            username: this.props.currentUser,
+            comment: ''
+        }
 
-    //     this.updateMessageState = this.updateMessageState.bind(this);
-    // }
+        this.updateMessageState = this.updateMessageState.bind(this);
+        this.updateUserName = this.updateUserName.bind(this);
 
-    // updateMessageState(event){
-    //     this.setState({comment: event.target.value })
-    // }
+    }
+
+    updateMessageState(event){
+        this.setState({comment: event.target.value })
+    }
+
+    updateUserName(event){
+        this.setState({username: event.target.value })
+        event.target.value = this.state.username;
+    }
 
     _handleKeyPress = (event) => {
     
         if (event.key === 'Enter') {
             const newMessage = {
-                username: event.target.previousSibling.value,
-                content: event.target.value
+                username: this.state.username,
+                content: this.state.comment
             }
 
             event.target.value = "";
@@ -34,8 +41,9 @@ class ChatBar extends Component {
     render() {
         return (
             <footer className="chatbar">
-                <input className="chatbar-username" name="username" placeholder="Your Name (Optional)" value={this.props.currentUser}/>
-                <input className="chatbar-message" name="message" placeholder="Type a message and hit ENTER" onKeyPress={this._handleKeyPress} />
+                {/* <input className="chatbar-username" name="username" placeholder="Your Name (Optional)" value={this.props.currentUser}/> */}
+                <input className="chatbar-username" name="username" placeholder="Your Name (Optional)" onChange={this.updateUserName} value={this.state.username}/>
+                <input className="chatbar-message" name="message" placeholder="Type a message and hit ENTER" onChange={this.updateMessageState} onKeyPress={this._handleKeyPress} />
             </footer>
         )
     }
